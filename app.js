0f04b098839354e4ed84096c83f5830004bace8a
nehansh100502 <nehansh100502@gmail.com>
2024-03-15 15:41:30 +0530

    fix cors problem

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 if(process.env.NODE_ENV !== "production"){
     require("dotenv").config({path: "config/config.env"});
 }
 // Using middlewares
+app.use(cors());
+const options = {
+    origin: 'http://localhost:4000',
+    }
+app.use(cors(options));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
