ea42e9f68398b6570ea0e843f6d4892fc0c3bfdc
nehansh100502 <nehansh100502@gmail.com>
2024-05-12 13:02:22 +0530

    modified in config

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const mongoose =require("mongoose");
 
 exports.connectDatabase = () =>{
-    mongoose
-    .connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
     .then((con)=>{ 
         console.log(`Database Connected: ${con.connection.host}`)
     })
