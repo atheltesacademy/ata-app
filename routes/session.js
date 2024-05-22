4d2af5a2bfd7b759d869776fe652b180629b6e07
Prajjwal Singh <prjjwls@gmail.com>
2024-05-22 06:00:38 +0530

    update apssword and change password apis endpoint name and method changes

--- routes/session.js
+++ routes/session.js
@@ -4,8 +4,8 @@ const session = require('../controllers/session');
 // const auth = require('../middlewares/auth'); 
 
 router.post('/login', session.login);
-router.put('/updatePassword', session.updatePassword);
-router.put('/forgotPassword,', session.forgotPassword);
+router.put('/update-password', session.updatePassword);
+router.post('/forgot-password,', session.forgotPassword);
 
 // Routes for sessions
 router.post('/signup', session.signup);
